672d97cf864ef2f6d7bde8d660308635f57078dd
Guy T Dev <guytdev@gmail.com>
2024-01-10 09:10:50 +0200

    responsive Homepage  working

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
-import { Home } from "./_root/pages";
+import { AllUsers, CreatePost, EditPost, Explore, Home, LikedPosts, PostDetails, Profile, Saved, UpdateProfile } from "./_root/pages";
+import { Toaster } from "./components/ui/toaster";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
-import { Toaster } from "./components/ui/toaster";
 const App = () => {
   return (
     <main className="flex h-screen">
@@ -18,6 +18,15 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path='/explore' element={<Explore />} />
+          <Route path='/saved' element={<Saved />} />
+          <Route path='/all-users' element={<AllUsers />} />
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:id' element={<EditPost />} />
+          <Route path='/posts/:id' element={<PostDetails />} />
+          <Route path='/profile/:id/*' element={<Profile />} />
+          <Route path='/update-profile/:id' element={<UpdateProfile />} />
+          <Route path='/likedposts/:id' element={<LikedPosts />} />
         </Route>
       </Routes>
       <Toaster />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { QueryProvider } from "./lib/react-query/QueryProvider";
+import { AuthProvider } from "./context/AuthContext";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement as HTMLElement);
 root.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
 );
